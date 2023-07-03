acdf5e8aead1953e62d257a43a8747aab7d0297e
Mathieu Beliveau <mathieu.beliveau.1@gmail.com>
2023-07-03 00:15:36 -0400

    allow to recompose folders

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import concurrent.futures
+import os
+import re
 import subprocess
 from argparse import ArgumentParser
 from datetime import datetime
@@ -55,10 +57,34 @@ def get_folders_to_process(corpus_path: Path) -> list[Path]:
     return list(filter(lambda sub_path: sub_path.is_dir() and not is_processed(sub_path), corpus_path.iterdir()))
 
 
+def recompose_folders():
+    base_path = Path(CORPUS_LOCATION)
+    result_dirs = base_path.glob("*_*-*")
+    range_match_expression = r"(.*)_(\d*)-(\d*)"
+    file_page_match_expression = f"(.*)_(\d*-\d*).pdf_(\d*)"
+    for dir in result_dirs:
+        range_match = re.search(range_match_expression, dir.name).groups()
+        source_dir = range_match[0]
+        start_range = int(range_match[1])
+        dir_files = dir.glob("*_*")
+        for file in dir_files:
+            file_matches = re.search(file_page_match_expression, file.name)
+            if file_matches is None:
+                continue
+            file_group_matches = file_matches.groups()
+            page = int(file_group_matches[2])
+            prefix = file_group_matches[0]
+            absolute_page = page + start_range - 1
+            new_page_name = f"{prefix}.pdf_{str(absolute_page).zfill(6)}"
+            os.rename(str(file.absolute()), f"{str(dir.parent)}/{source_dir}/{new_page_name}")
+
 if __name__ == '__main__':
     argparse = ArgumentParser()
     argparse.add_argument("-c", "--corpus", default='/home/strav/Dev/shared-corpus/')
+    argparse.add_argument("-r", "--recompose", default=False, type=bool)
     args = argparse.parse_args()
+    if args.recompose is True:
+        recompose_folders()
     base_path = Path(CORPUS_LOCATION)
     process_dirs_for_ocr(base_path)
 
